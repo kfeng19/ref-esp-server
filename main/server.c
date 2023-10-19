1dc302e073616c0fe520c56ea7db79d8ad78f9d9
Kaijun Feng <akahs0913@gmail.com>
2023-10-18 21:58:49 -0700

    clean up

--- main/server.c
+++ main/server.c
@@ -1,8 +1,7 @@
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_event.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "esp_system.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include <esp_http_server.h>
