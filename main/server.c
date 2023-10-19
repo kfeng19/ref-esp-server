3c08c2c09c610e85b840b9e7686287f47c1924eb
Kaijun Feng <akahs0913@gmail.com>
2023-10-18 21:55:46 -0700

    cleanup

--- main/server.c
+++ main/server.c
@@ -349,27 +349,6 @@ static const httpd_uri_t ctrl = {
 };
 
 
-// static void event_handler(void* arg, esp_event_base_t event_base,
-//                                 int32_t event_id, void* event_data)
-// {
-//     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-//         esp_wifi_connect();
-//     } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-//         if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-//             esp_wifi_connect();
-//             s_retry_num++;
-//             ESP_LOGI(TAG, "retry to connect to the AP");
-//         } else {
-//             xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-//         }
-//         ESP_LOGI(TAG,"connect to the AP fail");
-//     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-//         ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-//         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-//         s_retry_num = 0;
-//         xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-//     }
-// }
 static httpd_handle_t start_webserver(void)
 {
     httpd_handle_t server = NULL;
